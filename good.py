e1f1785c97bd04e9338b4c9094047685344963da
trushbin <135510877+trushbin@users.noreply.github.com>
2025-07-07 15:21:16 +0800

    Add files via upload

---
+++ good.py
@@ -0,0 +1,105 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+def autopad(k, p=None, d=1):  # kernel, padding, dilation
+    """Pad to 'same' shape outputs."""
+    if d > 1:
+        k = d * (k - 1) + 1 if isinstance(k, int) else [d * (x - 1) + 1 for x in k]  # actual kernel-size
+    if p is None:
+        p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
+    return p
+
+
+class Conv(nn.Module):
+    """Standard convolution with args(ch_in, ch_out, kernel, stride, padding, groups, dilation, activation)."""
+
+    default_act = nn.SiLU()  # default activation
+
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
+        """Initialize Conv layer with given arguments including activation."""
+        super().__init__()
+        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
+        self.bn = nn.BatchNorm2d(c2)
+        self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
+
+    def forward(self, x):
+        """Apply convolution, batch normalization and activation to input tensor."""
+        return self.act(self.bn(self.conv(x)))
+
+    def forward_fuse(self, x):
+        """Perform transposed convolution of 2D data."""
+        return self.act(self.conv(x))
+
+
+class ADown(nn.Module):
+    """ADown."""
+
+    def __init__(self, c1, c2):
+        """Initializes ADown module with convolution layers to downsample input from channels c1 to c2."""
+        super().__init__()
+        self.c = c2 // 2
+        self.cv1 = Conv(c1 // 2, self.c, 3, 2, 1)
+        self.cv2 = Conv(c1 // 2, self.c, 1, 1, 0)
+
+    def forward(self, x):
+        """Forward pass through ADown layer."""
+        x = torch.nn.functional.avg_pool2d(x, 2, 1, 0, False, True)
+        x1, x2 = x.chunk(2, 1)
+        x1 = self.cv1(x1)
+        x2 = torch.nn.functional.max_pool2d(x2, 3, 2, 1)
+        x2 = self.cv2(x2)
+        return torch.cat((x1, x2), 1)
+
+
+class FocusFeature(nn.Module):
+    def __init__(self, inc, kernel_sizes=(5, 7, 9, 11), e=0.5) -> None:
+        super().__init__()
+        hidc = int(inc[1] * e)
+        
+        self.conv1 = nn.Sequential(
+            nn.Upsample(scale_factor=2),
+            Conv(inc[0], hidc, 1)
+        )
+        self.conv2 = Conv(inc[1], hidc, 1) if e != 1 else nn.Identity()
+        self.conv3 = ADown(inc[2], hidc)
+        
+        
+        self.dw_conv = nn.ModuleList(nn.Conv2d(hidc * 3, hidc * 3, kernel_size=k, padding=autopad(k), groups=hidc * 3) for k in kernel_sizes)
+        self.pw_conv = Conv(hidc * 3, hidc * 3)
+    
+    def forward(self, x):
+        x1, x2, x3 = x
+        x1 = self.conv1(x1)
+        x2 = self.conv2(x2)
+        x3 = self.conv3(x3)
+        
+        x = torch.cat([x1, x2, x3], dim=1)
+        feature = torch.sum(torch.stack([x] + [layer(x) for layer in self.dw_conv], dim=0), dim=0)
+        feature = self.pw_conv(feature)
+        
+        x = x + feature
+        return x
+    
+class Attention(nn.Module):
+    def __init__(self, dim, num_heads=8, attn_ratio=0.5, r=8) -> None:
+        self.num_heads = num_heads
+        self.head_dim = dim // num_heads
+        self.key_dim = int(self.head_dim * attn_ratio)
+        self.scale = self.key_dim**-0.5
+        self.x_pool = nn.AdaptiveAvgPool2d((1, None))
+        self.y_pool = nn.AdaptiveAvgPool2d((None, 1))
+        self.conv1 = Conv(dim, dim//r, 1)
+        self.conv2 = Conv(dim//r, dim, 1)
+        self.sigmoid = nn.Sigmoid()
+        self.proj = Conv(dim, dim, 3)
+
+    def forward(self, x):
+        q = x
+        k = self.sigmoid(self.conv2(self.conv1(self.x_pool(x) + self.y_pool(x)))) * x
+        v = abs(q - k)
+        attn = (q.transpose(-2, -1) @ k) * self.scale
+        attn = attn.softmax(dim=-1)
+        x = (v @ attn.transpose(-2, -1))
+        x = self.proj(x) + x
+        return x
